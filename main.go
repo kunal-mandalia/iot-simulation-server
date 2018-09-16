779ff0c6e789e1a813da0284680b663f7be458b6
kunal-mandalia <kunal.v.mandalia@gmail.com>
2018-09-16 23:39:57 +0100

    feat: add start, stop, status handlers

--- main.go
+++ main.go
@@ -4,21 +4,79 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"./simulator"
 )
 
 const defaultPort = "8080"
 
-func pad(message string) string {
-	return "\n" + message + "\n\n"
+func formatMessage(message string) string {
+	return "\n    " + message + "\n\n"
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	status := pad("IoT Simulation Server is running")
+	status := formatMessage("IoT Simulation Server is running")
 	w.Write([]byte(status))
 }
 
+func startSimulationHandler(w http.ResponseWriter, r *http.Request) {
+	simulation := simulator.GetInstance()
+	if simulation.IsRunning == true {
+		message := "A simulation is already running. Go to /status for details or /stop to stop the current simulation"
+		w.WriteHeader(http.StatusLocked)
+		w.Write([]byte(formatMessage(message)))
+	} else {
+		sensorName := "Gyroscope"
+		measurementName := "Rotation"
+		logValueChange := func(simulation *simulator.Simulation) {
+			log.Print("Simulated value changed:")
+			log.Print(simulation)
+		}
+
+		if r.URL.Query().Get("sensor") != "" {
+			sensorName = r.URL.Query().Get("sensor")
+		}
+		if r.URL.Query().Get("measurement") != "" {
+			measurementName = r.URL.Query().Get("measurement")
+		}
+		simulator.SetProperties(sensorName, measurementName)
+		go simulator.Start(logValueChange)
+
+		message := "Started " + sensorName + " simulation for measurement " + measurementName
+		log.Print(message)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(formatMessage(message)))
+
+	}
+}
+
+func stopSimulationHandler(w http.ResponseWriter, r *http.Request) {
+	simulator.Stop()
+	message := "Stopped simulation"
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(formatMessage(message)))
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	simulation := simulator.GetInstance()
+	var message string
+
+	if simulation.IsRunning == true {
+		message = "Simulation is running for Sensor: " + simulation.Sensor.Name + " on Measurement: " + simulation.Sensor.Measurement.Name
+	} else {
+		message = "Simulation is not running"
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(formatMessage(message)))
+}
+
 func main() {
+	simulator.GetInstance()
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/start", startSimulationHandler)
+	http.HandleFunc("/stop", stopSimulationHandler)
+	http.HandleFunc("/status", statusHandler)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
